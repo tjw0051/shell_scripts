4c43eac3fed288b3918510043c45c5a6cca89000
Tom <t_j_w@hotmail.co.uk>
2018-05-12 13:40:19 +0100

    Added tmux grid functions

---
+++ tmux.sh
@@ -0,0 +1,26 @@
+
+tmux-grid () {
+
+if [ "$1" = "4" ]; then
+	echo "Creating 4-square tmux grid..."
+	tmux new-session \; \
+	split-window -v \; \
+	split-window -h \; \
+	select-pane -t 0 \; \
+	split-window -h \; \
+	select-pane -t 0 \;
+elif [ "$1" = "6" ]; then
+	echo "Creating 6-square tmux grid..."
+	tmux new-session \; \
+	split-window -h \; \
+	split-window -h \; \
+	select-layout even-horizontal \; \
+	split-window -v \; \
+	select-pane -t 1 \; \
+	split-window -v \; \
+	select-pane -t 0 \; \
+	split-window -v \; \
+	select-pane -t 0 \;
+
+fi
+}
